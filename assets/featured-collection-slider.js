68054605c645ec5607fae5ff6745d2707f1505ff
churchywork <callumchurch8.work@gmail.com>
2024-11-28 18:56:27 +0000

    Product slide styling and responsive.
    - Added responsive JS to the slider
    - Added basic product styling using values from Figma
    - Added bootstrap style container and spacing below header bar

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -3,7 +3,27 @@ $(document).ready(function () {
       autoplay: false,
       dots: true,
       arrows: false,
-      slidesToShow: 4
+      slidesToShow: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3     
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,      
+          settings: {
+            slidesToShow: 1     
+          }
+        }
+      ]
     });
 });
   
