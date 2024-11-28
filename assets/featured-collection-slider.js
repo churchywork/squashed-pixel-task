d55edc014ec01dfce5cb748e214a314907b9f418
churchywork <callumchurch8.work@gmail.com>
2024-11-28 18:18:57 +0000

    Created test page and liquid structure and slick slider js
    - Created a page template to host the slider
    - Created products and collection with placeholder info to display info
    - HTML setup with product card containing dynamic product info
    - Slick JS setup

---
+++ assets/featured-collection-slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.featured-collection-slider').slick({
+      autoplay: false,
+      dots: true,
+      arrows: false,
+      slidesToShow: 4
+    });
+});
+  
