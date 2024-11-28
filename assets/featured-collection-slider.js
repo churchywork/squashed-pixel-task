f352c6b002057df6dc4477677df0f6028f20a2b8
churchywork <callumchurch8.work@gmail.com>
2024-11-28 19:44:09 +0000

    Add attribute to allow autoplay to toggle on the slider

--- assets/featured-collection-slider.js
+++ assets/featured-collection-slider.js
@@ -1,29 +1,31 @@
 $(document).ready(function () {
-    $('.featured-collection-slider').slick({
-      autoplay: false,
+  var autoplaySetting = $('.featured-collection-slider').data('autoplay') === true;
+
+  $('.featured-collection-slider').slick({
+      autoplay: autoplaySetting,
+      autoplaySpeed: 3000,
       dots: true,
       arrows: false,
       slidesToShow: 4,
       responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3     
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3
+              }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                  slidesToShow: 2
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1
+              }
           }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2
-          }
-        },
-        {
-          breakpoint: 480,      
-          settings: {
-            slidesToShow: 1     
-          }
-        }
       ]
-    });
+  });
 });
-  
